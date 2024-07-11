dfb710fc5ff1b899e243e87f5a9023d99be0eee8
JinHoooooou <jinho4744@naver.com>
2024-07-11 15:55:08 +0900

    update programmers problems (24-07-09)

--- src/programmers/level2/큰_수_만들기_0709/BigNumber.java
+++ src/programmers/level2/큰_수_만들기_0709/BigNumber.java
@@ -1,25 +1,26 @@
 package programmers.level2.큰_수_만들기_0709;
 
+import java.util.Stack;
+
 public class BigNumber {
 
   public String solution(String number, int k) {
     StringBuilder result = new StringBuilder();
-    int resultLength = number.length() - k;
-    int leftIndex = 0;
-    int rightIndex = number.length() - resultLength;
-    while (result.length() < resultLength) {
-      int max = 0;
-      for (int i = leftIndex; i <= rightIndex; i++) {
-        int target = Character.getNumericValue(number.charAt(i));
-        if (target > max) {
-          max = target;
-          leftIndex = i + 1;
-        }
+    Stack<Character> stack = new Stack<>();
+
+    for (int i = 0; i < number.length(); i++) {
+      char digit = number.charAt(i);
+      while (!stack.isEmpty() && k > 0 && digit > stack.peek()) {
+        stack.pop();
+        k--;
       }
-      result.append(max);
-      rightIndex++;
+      stack.push(digit);
     }
 
-    return result.toString();
+    while (!stack.isEmpty()) {
+      result.append(stack.pop());
+    }
+
+    return result.reverse().toString();
   }
 }
